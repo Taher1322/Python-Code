a249ad27a895f08efdfbc8972078374122758e80
Taher1322 <46395731+Taher1322@users.noreply.github.com>
2021-05-12 19:16:47 +0530

    thingspeak.py

--- thingspeak_v1.py
+++ thingspeak_v1.py
@@ -2,13 +2,13 @@ from time import time, sleep
 from urllib.request import urlopen
 import sys
 
-WRITE_API = "UE6UU8JFDCIHLVKT" # Replace your ThingSpeak API key here
+WRITE_API = "XXXX" # Replace your ThingSpeak API key here
 BASE_URL = "https://api.thingspeak.com/update?api_key={}".format(WRITE_API)
 
 
 try:
     while True:
-              
+        #Sending some static values in the field of Thingspeak platform      
         humidity = 70.54
         temperature = 27.65
         thingspeakHttp = BASE_URL + "&field1={:.2f}&field2={:.2f}".format(temperature, humidity)
