b4edcf47202b25a1dd1404a6c888c31bc55fb423
Taher1322 <46395731+Taher1322@users.noreply.github.com>
2021-05-12 18:40:20 +0530

    Send_email.py

---
+++ mail.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+ 
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+from email.mime.image import MIMEImage
+
+GPIO.setmode(GPIO.BCM) 
+
+IRsensor =  21
+GPIO.setup(IRsensor, GPIO.IN)
+ 
+fromaddr = "test2718test@gmail.com"    # change the email address accordingly
+toaddr = "electrofunedu@gmail.com"
+ 
+mail = MIMEMultipart()
+ 
+mail['From'] = fromaddr
+mail['To'] = toaddr
+mail['Subject'] = "Door Status"
+body = "Please find the Door status "
+ 
+ 
+def sendMail():
+    mail.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "Your password here")
+    text = "Door Open"
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+ 
+
+ 
+while (True):
+    if GPIO.input(IRsensor):
+        sendMail()
+    else:
+        time.sleep(0.01)
